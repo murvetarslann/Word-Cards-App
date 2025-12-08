f7c7ff37dd78765cc57af45b84f72f3ed0f63abc
Mürvet Arslan <murvet.arslan01@gmail.com>
2025-12-08 12:21:45 +0300

    API bağlantısı için HTTP isteklerine izin verildi

--- WordCardsApp/ViewModel/AddWordViewController.swift
+++ WordCardsApp/ViewModel/AddWordViewController.swift
@@ -16,12 +16,24 @@ class AddWordViewController: UIViewController {
 
     }
     
+    func makeAlert(titleInput: String, messageInput: String) {
+        let alert = UIAlertController(title: titleInput, message: messageInput, preferredStyle: UIAlertController.Style.alert)
+        let okeButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+        alert.addAction(okeButton)
+        self.present(alert, animated: true, completion: nil)
+    }
+    
     @IBAction func englishBringButtonClicked(_ sender: Any) {
-        
+        guard let text = turkishWordTextField.text, !text.isEmpty else {
+            makeAlert(titleInput: "Error!", messageInput: "You did not enter any Turkish words!")
+            return 
+        }
     }
     
     @IBAction func wordSaveButtonClicked(_ sender: Any) {
         
     }
+    
+   
     
 }
